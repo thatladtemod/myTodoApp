75bb9ebf0dd99c836daa9cbbaff436ad0bb30034
Omoniyi Temitayo <temit3684@gmail.com>
2025-02-16 10:47:29 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,44 @@
+const inputBoxValue = document.getElementById("inputBox")
+const todoList = document.querySelector(".todoList")
+const message = document.querySelector("p")
+
 function addTodo() {
-  const inputBoxValue = document.getElementById("inputBox");
-  const todoList = document.querySelector(".todoList");
-  const message = document.querySelector("p");
 
   if (inputBoxValue.value === "") {
-    message.textContent = "Please, enter a valid task.";
-
-    setTimeout(() => message.remove(), 3000);
+    message.textContent = "Please, enter a valid task."
   } else {
-    const li = document.createElement("li");
-    li.classList.add("added");
-    li.innerHTML = `${inputBoxValue.value} <div><img src="images/delete-button-svgrepo-com.svg" alt=""></div>`;
-    todoList.appendChild(li);
+    const li = document.createElement("li")
+    li.classList.add("added")
+    li.innerHTML = `${inputBoxValue.value} <div class="delete"><img src="images/delete-button-svgrepo-com.svg" alt=""></div>`;
+    todoList.appendChild(li)
 
-    inputBoxValue.value = "";
+    message.remove()
+
+    li.querySelector(".delete").addEventListener("click", deletetask)
+
   }
+
+    inputBoxValue.value = ""
+    saveData()
 }
+
+function deletetask(e) {
+  const deleteButton = e.target.closest(".delete")
+  const listItem = deleteButton.closest("li")
+  listItem.remove()
+  saveData()
+}
+
+function saveData() {
+  localStorage.setItem("data", todoList.innerHTML)
+}
+
+function displayItems() {
+  todoList.innerHTML = localStorage.getItem("data")
+  const deleteButtons = todoList.querySelectorAll(".delete")
+  deleteButtons.forEach(button => {
+    button.addEventListener("click", deletetask)
+  })
+}
+
+displayItems()
